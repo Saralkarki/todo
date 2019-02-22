b64951a1232b9164ad89fd04f3aef4d36e71a9e6
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-02-22 11:05:18 +0545

    chaged the create method to save via new and save method.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,9 +1,21 @@
 class TodoItemsController < ApplicationController
     before_action :set_todo_list
     def create
-        @todo_item = @todo_list.todo_items.create(todo_items_params)
-        redirect_to @todo_list
+        @todo_item = @todo_list.todo_items.new(todo_items_params)
+    respond_to do |format|  
+        if @todo_item.save
+            format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
+            format.json { render :show, status: :created, location: @todo_list }
+        else
+            format.html { redirect_to @todo_list, notice: 'Could not save' }
+            format.json { render :show, status: :created, location: @todo_list }
+        end
     end
+    end
+    # def create
+    #     @todo_item = @todo_list.todo_items.create(todo_items_params)
+    #     redirect_to @todo_list
+    # end
 
     def destroy
         @todo_item = @todo_list.todo_items.find(params[:id])
@@ -18,7 +30,7 @@ class TodoItemsController < ApplicationController
     private 
     def set_todo_list
         @todo_list = TodoList.find(params[:todo_list_id])
-        # puts @todo_list
+        # puts ">>>>>>" @todo_list
     end
 
     def todo_items_params 
